a3d882c7c3d64abb4b5e4e241a302e1413e10964
neelkumar30 <league@WTS-IM-6.attlocal.net>
2017-08-22 14:30:55 -0700

    solving algorithms

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,7 +4,45 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0; //<- this needs changing
+		int broken = 0;
+		for(String cracked : eggs) {
+			
+			if (cracked.contains("cracked")) {
+					return broken;
+			}
+			 broken ++;
+		}
+		
+		return 0;
+
+			
+		
+		 //<- this needs changing
+	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		int pearls = 0;
+		for(Boolean found : oysters) {
+			
+			if (found.equals(true)) {
+				return pearls;
+			}
+			pearls++;
+		}
+		return 0;
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		double person = 0;
+		for(Double tallest : peeps) {
+			peeps.indexOf(tallest);
+			if (tallest>person) {
+				person = tallest;
+	
+			}
+		
+		}
+		return person;
 	}
 	
 	//Add other methods here
